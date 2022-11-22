10b7462459ce1ed75c5396859eaf346661f46535
Magdy Elsayyad <medoelsayad229@gmail.com>
2022-11-22 19:42:44 +0200

    habits

--- data/psyco-habits.js
+++ data/psyco-habits.js
@@ -26,20 +26,88 @@ const positiveHabits = [
   },
 ];
 
+
+const negativeHabits = [
+  {
+      title: 'السهر وقلة النوم ❌',
+      desc: `
+حلول بسيطه جداً 
+بلاش تشرب كفايين فى وقت متاخر زي (الشاي او القهوه ) .  
+
+مارس رياضة بدنية قبل متنام بأربعة  ساعات على الأقل  .
+
+ابعد عن التفكير وانت جالس على السرير  .
+
+مارس تمارين التأمل لأنها بتخلص من التوتر عشان تعرف تنام بعمق .`,
+      img: '1'
+  },
+  {
+      title: 'التفكير السلبى ❌',
+      desc: `
+ازاى تتخلص من التفكير السلبي تابع معانا عشان تعرف تعالج نفسك من التفكير السلبى اتكلم مع هذه الافكار اتعرف عليها قل لها توقفى 
+ولو شعرت بفكرة سيئه وقفها من البدايه متخلهاش تسيطر عليك ودا هيمنعك من ولادة افكار كثيرة .
+ومهم جدا نمارس رياضة .
+والتأمل والاسترخاء .`,
+      img: '2'
+  },
+  {
+      title: 'الندم على الماضى ❌',
+      desc: `
+الندم على الماضى ممكن يكون طريقه بتتعلم بيها فى المستقبل لكن المهم انك تكون متصالح مع تجارب الماضى حاول تحوله من ندم لطريقه تستفيد بيها دايماً فى المستقبل ودا بنسميه ندم ايجابى حط دايماً فى بالك إن كل الناس بتغلط لان دي طبيعة البشر حول شعور الندم الى شعور امتنان 
+سامح نفسك دا هيخليك عندك ثقه فى نفسك حط لنفسك جمل تشجيعيه عشان ترفع من حالتك `,
+      img: '3'
+  },
+  {
+      title: 'الاستخدام المفرط لوسائل التواصل الاجتماعى ❌',
+      desc: `
+عشان تتخلص من ادمان وسائل التواصل الاجتماعى ابدأ اتعلم حاجه جديده زى مثلاً ممارسة لغة جديده او قراءة كتاب جديد او اى نشاط مفيد .`,
+      img: '4'
+  },
+  {
+      title: 'الكسل وعدم ممارسه التمارين الرياضية ❌',
+      desc: `
+هبدأ الشهر الجاى وحجج كتير بنقنع نفسنا بيها اتبع هذه النصائح للتخلص من الكسل :  
+١-حدد الهدف من ممارسة الرياضة  .
+٢-حط جدول يومى لنفسك  .
+٣- استمتع بها ومتخسش انها واجب عليك لازم تخلصه  .
+٤-افتكر ديما فوائد الرياضة واعتبرها حافز بيشجعك تستمر .`,
+      img: '5'
+  },
+ 
+]
+
 const habitsHtml = positiveHabits.map((e, i) => {
   return `
     
-
-<div class="row ${i %2 ===0 ? 'flex-row-reverse': ''} justify-content-center  g-0 mb-2-9">
-<div class="col-md-5 col-xl-4 bg-img cover-background min-height-350 border-start-md-radius-10 border-top-sm-radius-10"
-    data-background="../img/positive-habits/${e.img}.jpg"></div>
+  <div class="row align-items-center ${i %2 ===0 ? 'flex-row-reverse': ''} justify-content-center  g-0 mb-2-9">
+  <div class="col-md-5 col-xl-4 bg-img cover-background min-height-350 ${i %2 ===0 ? 'border-end-md-radius-10 border-bottom-sm-radius-10': 'border-start-md-radius-10 border-top-sm-radius-10'} "
+      data-background="../img/negative-habits/${e.img}.jfif"></div>
+  <div class="col-md-7 col-xl-6">
+  <div class="author">
+  <h4 class="display-27 font-weight-800 mb-0 text-center"> ${e.title}</h4>
+</div>
+      <div class="success-content border-end-md-radius-10 border-bottom-sm-radius-10">
+          <p>
+          ${e.desc}</p>
+         
+      </div>
+  </div>
+  </div>
+      `;
+});
+const negativeHabitsHtml = negativeHabits.map((e, i) => {
+  return `
+<div class="row align-items-center   ${i %2 ===0 ? 'flex-row-reverse': ''} justify-content-center  g-0 mb-2-9">
+<div class="col-md-5 col-xl-4 bg-img cover-background min-height-350 ${i %2 ===0 ? 'border-end-md-radius-10 border-bottom-sm-radius-10': 'border-start-md-radius-10 border-top-sm-radius-10'} "
+    data-background="../img/negative-habits/${e.img}.jfif"></div>
 <div class="col-md-7 col-xl-6">
+<div class="author">
+<h4 class="display-27 font-weight-800 mb-0 text-center"> ${e.title}</h4>
+</div>
     <div class="success-content border-end-md-radius-10 border-bottom-sm-radius-10">
         <p>
         ${e.desc}</p>
-        <div class="author">
-            <h4 class="display-27 font-weight-800 mb-1"> ${e.title}</h4>
-        </div>
+      
     </div>
 </div>
 </div>
@@ -47,7 +115,12 @@ const habitsHtml = positiveHabits.map((e, i) => {
 });
 
 window.onload = () => {
-  document.getElementById("habitsWrapper").innerHTML = habitsHtml.join("");
+  if(document.getElementById("habitsWrapper")){
+    document.getElementById("habitsWrapper").innerHTML = habitsHtml.join("");
+  }
+  if(document.getElementById("negativeHabitsHtml")){
+    document.getElementById("negativeHabitsHtml").innerHTML = negativeHabitsHtml.join("");
+  }
   // sections background image from data background
   var pageSection = $(".parallax,.bg-img");
   pageSection.each(function (indx) {
