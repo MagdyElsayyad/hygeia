77c1a76ca9a748a3ec337731b3dea7460667b6aa
Magdy Elsayyad <medoelsayad229@gmail.com>
2022-11-22 19:27:11 +0200

    ح-اشلاهفس

---
+++ data/food-wasafat.js
@@ -0,0 +1,426 @@
+const foods = [
+    {
+        title: '',
+        img:'',
+        text: ``,
+        ways: [
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+        ]
+    },
+    // ==========================================
+    {
+        title: '',
+        img:'',
+        text: ``,
+        ways: [
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+        ]
+    },
+    // ==========================================
+    {
+        title: '',
+        img:'',
+        text: ``,
+        ways: [
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+        ]
+    },
+    // ==========================================
+    {
+        title: '',
+        img:'',
+        text: ``,
+        ways: [
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+        ]
+    },
+    // ==========================================
+    {
+        title: '',
+        img:'',
+        text: ``,
+        ways: [
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+        ]
+    },
+    // ==========================================
+    {
+        title: '',
+        img:'',
+        text: ``,
+        ways: [
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+        ]
+    },
+    // ==========================================
+    {
+        title: '',
+        img:'',
+        text: ``,
+        ways: [
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+        ]
+    },
+    // ==========================================
+    {
+        title: '',
+        img:'',
+        text: ``,
+        ways: [
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+            {
+                title: '',
+                desc: '',
+                img: ''
+            },
+        ]
+    },
+    // ==========================================
+]
